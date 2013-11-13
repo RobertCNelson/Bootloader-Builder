1a2901af52118878b0e5d64092b09b3d1be733dc
Robert Nelson <robertcnelson@gmail.com>
2013-11-13 10:37:10 -0600

    merge to: https://github.com/RobertCNelson/u-boot/commit/63c4f17b2f8017d22241522a48c765073b8791b0
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -37,8 +37,8 @@ stable_at91bootstrap_sha="16901eba66246899cb86f3c3364426a44d7e63de"
 uboot_stable="v2013.07"
 uboot_testing="v2013.10"
 
-#uboot_latest="e5a9a4076f1fb9fb9ce53c2aec32422073bbc66a"
-uboot_latest="2cee0408e5c6f5dbdfa89fb40168ba2ead3f61a6"
+#uboot_latest="2cee0408e5c6f5dbdfa89fb40168ba2ead3f61a6"
+uboot_latest="63c4f17b2f8017d22241522a48c765073b8791b0"
 
 unset GIT_OPTS
 unset GIT_NOEDIT
