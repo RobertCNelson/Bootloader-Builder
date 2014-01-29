82f08d3ba32bafe8179275a2ef1bce3c5f967226
Robert Nelson <robertcnelson@gmail.com>
2014-01-29 16:48:03 -0600

    omap3_beagle: omap3-beagle-xm-ab.dtb
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -249,8 +249,9 @@ build_u_boot () {
 	if [ "${testing}" ] ; then
 		#r1: initial release
 		#r2: beagle: loadaddr=0x80300000
-		#r3: (pending)
-		RELEASE_VER="-r2" #bump on every change...
+		#r3: beagle-xm: drop fixfdt (u-boot wants it done in kernel..)
+		#r4: (pending)
+		RELEASE_VER="-r3" #bump on every change...
 
 		#Atmel:
 		git am "${DIR}/patches/${uboot_patch_dir}/0001-at91sam9g20ek-uEnv.txt-bootz-n-fixes.patch"
