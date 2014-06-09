dd351ad09f0c9b60c067e84a0cf7b0dada0405a5
Robert Nelson <robertcnelson@gmail.com>
2014-06-09 16:33:47 -0500

    vf610twr: disable memory test for more room in sram
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -314,8 +314,9 @@ build_u_boot () {
 		#r1: initial release
 		#r2: am335x_evm: $fdtbase-$cape.dtb
 		#r3: am335x_evm: use Tom's Golden values...
-		#r4: (pending)
-		RELEASE_VER="-r3" #bump on every change...
+		#r4: vf610twr: we seem to have a sram limit (230kb fails to load)
+		#r5: (pending)
+		RELEASE_VER="-r4" #bump on every change...
 
 		#Atmel:
 		git am "${DIR}/patches/${uboot_patch_dir}/board/0001-at91sam9x5ek-fix-nand-init-for-Linux-2.6.39.patch"
