fa229fbccab604ec2d2af6167e50eb50a3ece33d
Robert Nelson <robertcnelson@gmail.com>
2014-10-14 09:38:51 -0500

    u-boot: v2014.10
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -4,6 +4,6 @@ stable_at91bootstrap_sha="496c0830c7f1fc7e9ce47a2ea16cb13c887caaa4"
 
 uboot_old="v2014.04"
 uboot_stable="v2014.07"
-uboot_testing="v2014.10-rc3"
+uboot_testing="v2014.10"
 
-uboot_latest="e89d623f099c44b0b166ccf46bce2e6a0b99c984"
+#uboot_latest="e89d623f099c44b0b166ccf46bce2e6a0b99c984"
