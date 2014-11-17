9650587b8b57909c640607eb25541417c822c6fd
Robert Nelson <robertcnelson@gmail.com>
2014-11-17 08:50:20 -0600

    merge to: https://github.com/RobertCNelson/u-boot/commit/2086e388d56cbb0e3737a6276f04f00f74bf6723
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -6,4 +6,4 @@ uboot_old="v2014.07"
 uboot_stable="v2014.10"
 uboot_testing="v2015.01-rc1"
 
-uboot_latest="63f7af9a4cfb4a3b9136674c6612c6d13957c2cd"
+uboot_latest="2086e388d56cbb0e3737a6276f04f00f74bf6723"
