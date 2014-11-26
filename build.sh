82391c4f173ac9fd154e824c39086498450dac61
Robert Nelson <robertcnelson@gmail.com>
2014-11-26 16:47:37 -0600

    u-boot: v2015.01-rc2
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -347,16 +347,12 @@ build_u_boot () {
 		RELEASE_VER="-r1" #bump on every change...
 		#halt_patching_uboot
 
+		git am "${DIR}/patches/${uboot_patch_dir}/0001-fs-ext4-ext4fs.c-fs-fs.c-fs-fat-fat_write.c-Adjust-6.patch"
+
 		#Atmel:
 		git am "${DIR}/patches/${uboot_patch_dir}/0001-at91sam9x5ek-uEnv.txt-bootz-n-fixes.patch"
 		git am "${DIR}/patches/${uboot_patch_dir}/0001-sama5d3xek-uEnv.txt-bootz-n-fixes.patch"
 		git am "${DIR}/patches/${uboot_patch_dir}/0001-sama5d3_xplained-uEnv.txt-bootz-n-fixes.patch"
-
-		#Atmel: sama5d4ek
-		git am "${DIR}/patches/${uboot_patch_dir}/board/0001-mtd-atmel_nand-runtime-to-build-gf-table-for-pmecc.patch"
-		git am "${DIR}/patches/${uboot_patch_dir}/board/0002-net-macb-enable-GMAC-IP-without-GE-feature-support.patch"
-		git am "${DIR}/patches/${uboot_patch_dir}/board/0003-ARM-atmel-add-sama5d4ek-board-support.patch"
-		git am "${DIR}/patches/${uboot_patch_dir}/board/0004-ARM-atmel-add-sama5d4-xplained-ultra-board-support.patch"
 		git am "${DIR}/patches/${uboot_patch_dir}/0001-sama5d4ek-uEnv.txt-bootz-n-fixes.patch"
 
 		#Freescale:
@@ -384,6 +380,8 @@ build_u_boot () {
 		#r2: (pending)
 		RELEASE_VER="-r1" #bump on every change...
 		#halt_patching_uboot
+
+		git am "${DIR}/patches/${uboot_patch_dir}/0001-fs-ext4-ext4fs.c-fs-fs.c-fs-fat-fat_write.c-Adjust-6.patch"
 
 		#Atmel:
 		git am "${DIR}/patches/${uboot_patch_dir}/0001-at91sam9x5ek-uEnv.txt-bootz-n-fixes.patch"
--- version.sh
+++ version.sh
@@ -4,6 +4,6 @@ stable_at91bootstrap_sha="e977bb2ad03530cb393e2389c191ba50ac4ba07d"
 
 uboot_old="v2014.07"
 uboot_stable="v2014.10"
-uboot_testing="v2015.01-rc1"
+uboot_testing="v2015.01-rc2"
 
-#uboot_latest="85bafb6da4dddfffa78479aa49a72ae48578a4ce"
+uboot_latest="85bafb6da4dddfffa78479aa49a72ae48578a4ce"
