0685aa7225c1b7c12a0aa944b209b60854dcae2f
Robert Nelson <robertcnelson@gmail.com>
2015-03-02 12:39:21 -0600

    merge to: https://github.com/RobertCNelson/u-boot/commit/ed78b11ca43b37d4cb4a1987cb67dcdd53a869b7
    https://github.com/RobertCNelson/u-boot/compare/47d8ae4069b47ce966c0c5e0d8dd041e69ee1f86...ed78b11ca43b37d4cb4a1987cb67dcdd53a869b7
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -6,5 +6,5 @@ uboot_old="v2014.10"
 uboot_stable="v2015.01"
 uboot_testing="v2015.04-rc2"
 
-uboot_prev="38dac81b3d0e777f301ca98100bfbcab01d616c2"
-uboot_latest="47d8ae4069b47ce966c0c5e0d8dd041e69ee1f86"
+uboot_prev="47d8ae4069b47ce966c0c5e0d8dd041e69ee1f86"
+uboot_latest="ed78b11ca43b37d4cb4a1987cb67dcdd53a869b7"
