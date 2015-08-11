94471cc17f9e51d18b40dd0044f0f19643264039
Robert Nelson <robertcnelson@gmail.com>
2015-08-11 12:01:17 -0500

    am335x_evm: support the beaglebone green
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -316,8 +316,9 @@ build_u_boot () {
 		#r1: initial release
 		#r2: am335x_evm: add tftp_dir prefix ${tftp_dir}${bootfile} & ${tftp_dir}dtbs/${fdtfile}
 		#r3: am335x_evm: dump reset reason...
-		#r4: (pending)
-		RELEASE_VER="-r3" #bump on every change...
+		#r4: am335x_evm: support the beaglebone green
+		#r5: (pending)
+		RELEASE_VER="-r4" #bump on every change...
 		#halt_patching_uboot
 
 		case "${board}" in
