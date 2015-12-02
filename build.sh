d48148d4b07a510a5a71bc7e88304f123f31de65
Robert Nelson <robertcnelson@gmail.com>
2015-12-02 09:17:37 -0600

    merge to: https://github.com/RobertCNelson/u-boot/commit/be30dfbabbee12770221f434d2aa08627e712b97
    https://github.com/RobertCNelson/u-boot/compare/4a421a67b6f36678bfd27b7fedc0de35faf94c76...be30dfbabbee12770221f434d2aa08627e712b97
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -634,8 +634,9 @@ build_u_boot () {
 		#r21: http://git.ti.com/gitweb/?p=ti-u-boot/ti-u-boot.git;a=commit;h=90c6ccc4970d0b783e5fcc42377ff5fc2402bb57
 		#r22: http://git.ti.com/gitweb/?p=ti-u-boot/ti-u-boot.git;a=commit;h=189c59c9a2f105b62ab2e195deeef18962cc8b67
 		#r23: http://git.ti.com/gitweb/?p=ti-u-boot/ti-u-boot.git;a=commit;h=110dfa44a2ea96e353d24ef4d21fa7756dcbba41
-		#r24: (pending)
-		RELEASE_VER="-r23" #bump on every change...
+		#r24: http://git.ti.com/gitweb/?p=ti-u-boot/ti-u-boot.git;a=commit;h=30d3ba04dd090d5fa9c4103cf620d74c2edd47e2
+		#r25: (pending)
+		RELEASE_VER="-r24" #bump on every change...
 
 		p_dir="${DIR}/patches/v2015.07"
 		echo "${git} \"${p_dir}/0001-beagle_x15-uEnv.txt-bootz-n-fixes.patch\""
--- version.sh
+++ version.sh
@@ -6,5 +6,5 @@ uboot_old="v2015.07"
 uboot_stable="v2015.10"
 uboot_testing="v2016.01-rc1"
 
-uboot_prev="fa8883a1e39a20e72aaa5093af0c80062cb95757"
-uboot_latest="4a421a67b6f36678bfd27b7fedc0de35faf94c76"
+uboot_prev="4a421a67b6f36678bfd27b7fedc0de35faf94c76"
+uboot_latest="be30dfbabbee12770221f434d2aa08627e712b97"
