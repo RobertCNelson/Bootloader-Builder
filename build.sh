3248c13767b995cd73518a911020cb410efbf207
Robert Nelson <robertcnelson@gmail.com>
2016-02-02 19:29:00 -0600

    omap5: use keyed autoboot
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -455,8 +455,9 @@ build_u_boot () {
 	p_dir="${DIR}/patches/${uboot_testing}"
 	if [ "${testing}" ] ; then
 		#r1: initial release
-		#r2: (pending)
-		RELEASE_VER="-r1" #bump on every change...
+		#r2: omap5_uevm: stray key on serial protect...
+		#r3: (pending)
+		RELEASE_VER="-r2" #bump on every change...
 		#halt_patching_uboot
 
 		case "${board}" in
