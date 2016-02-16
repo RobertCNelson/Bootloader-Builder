e4ecf2e2b15bcff75442ff6ced3fc366e8360e09
Robert Nelson <robertcnelson@gmail.com>
2016-02-16 08:59:47 -0600

    u-boot: v2016.03-rc2
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -4,7 +4,7 @@ stable_at91bootstrap_sha="533f4082e35869ab61d1112f518063aa0496febb"
 
 uboot_old="v2015.10"
 uboot_stable="v2016.01"
-uboot_testing="v2016.03-rc1"
+uboot_testing="v2016.03-rc2"
 
-uboot_prev="94985cc9d3d7ed991ca8b2627d5894df5ea68f49"
-uboot_latest="9ef2835f26652092a61b0cb0551ef4f36be27946"
+#uboot_prev="94985cc9d3d7ed991ca8b2627d5894df5ea68f49"
+#uboot_latest="9ef2835f26652092a61b0cb0551ef4f36be27946"
