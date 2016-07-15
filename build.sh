61b533fb9fd14208463bce1f39c57ea0ab1bd33f
Robert Nelson <robertcnelson@gmail.com>
2016-07-15 14:48:57 -0500

    v2016.07
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -742,8 +742,9 @@ build_u_boot () {
 		#r5: http://git.ti.com/gitweb/?p=ti-u-boot/ti-u-boot.git;a=commit;h=fc109e36469a3cf8eca245cd1cece517dde11e8f
 		#r6: http://git.ti.com/gitweb/?p=ti-u-boot/ti-u-boot.git;a=commit;h=b43cf45fec75961d196da4563bd32c0215d2fd58
 		#r7: reverse r6 config chagne
-		#r8: (pending)
-		RELEASE_VER="-r7" #bump on every change...
+		#r8: http://git.ti.com/gitweb/?p=ti-u-boot/ti-u-boot.git;a=commit;h=a4560200fc597109328a6454094fa77516434880
+		#r9: (pending)
+		RELEASE_VER="-r8" #bump on every change...
 
 		p_dir="${DIR}/patches/ti-2016.05"
 		echo "patch -p1 < \"${p_dir}/0001-beagle_x15-uEnv.txt-bootz-n-fixes.patch\""
