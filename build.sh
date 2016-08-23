89b42d5a77bb35ed1c2625b7a210c7ed0e4b4a84
Robert Nelson <robertcnelson@gmail.com>
2016-08-23 15:10:53 -0500

    am335x: enable CONFIG_OF_LIBFDT_OVERLAY
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -482,8 +482,9 @@ build_u_boot () {
 	p_dir="${DIR}/patches/${uboot_testing}"
 	if [ "${testing}" ] ; then
 		#r1: initial release
-		#r2: (pending)
-		RELEASE_VER="-r1" #bump on every change...
+		#r2: am335x_evm: enable CONFIG_OF_LIBFDT_OVERLAY
+		#r3: (pending)
+		RELEASE_VER="-r2" #bump on every change...
 		#halt_patching_uboot
 
 		case "${board}" in
