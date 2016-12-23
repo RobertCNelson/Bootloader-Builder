75032582c690e090b0d6152f92c87edb5f5e9bc7
Robert Nelson <robertcnelson@gmail.com>
2016-12-23 15:09:27 -0600

    v2017.01-rc2, dtb_overlay option
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -532,8 +532,9 @@ build_u_boot () {
 	if [ "${testing}" ] ; then
 		#r1: initial release
 		#r2: am335x_evm: arm: am33xx: Initialize EMIF REG_PR_OLD_COUNT for BBB and am335x-evm
+		#r3: am335x_evm: dtb_overlay=file.dtbo
 		#r3: (pending)
-		RELEASE_VER="-r2" #bump on every change...
+		RELEASE_VER="-r3" #bump on every change...
 		#halt_patching_uboot
 
 		case "${board}" in
