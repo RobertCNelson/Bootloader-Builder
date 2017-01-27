3bac3e53186da2128e245362cd99ca60528fc8db
Robert Nelson <robertcnelson@gmail.com>
2017-01-27 16:04:03 -0600

    am335x: hash capes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -431,8 +431,9 @@ build_u_boot () {
 		#r11: am335x_evm: auto load eMMC/HDMI
 		#r12: am335x_evm: blue fixes
 		#r13: am335x_evm: add uboot disable options
-		#r14: (pending)
-		RELEASE_VER="-r13" #bump on every change...
+		#r14: am335x_evm: hash capes for virtual detection...
+		#r15: (pending)
+		RELEASE_VER="-r14" #bump on every change...
 		#halt_patching_uboot
 
 		case "${board}" in
