060014281f215d57b45a11dc022135c65e281273
Robert Nelson <robertcnelson@gmail.com>
2017-06-19 22:11:15 -0500

    u-boot: v2017.07-rc2
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -4,7 +4,7 @@ stable_at91bootstrap_sha="533f4082e35869ab61d1112f518063aa0496febb"
 
     uboot_old="v2017.03"
  uboot_stable="v2017.05"
-uboot_testing="v2017.07-rc1"
+uboot_testing="v2017.07-rc2"
 
 #uboot_prev="4cf5c5f1e60e6541630ae829b5fa2db704c268f0"
 #uboot_latest="47895838a4fbe867ba9a170c6d1ea6a794095025"
