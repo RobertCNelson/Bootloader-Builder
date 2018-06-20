bcbd5b53d73cabd4a276cfba8b32426c82939956
Robert Nelson <robertcnelson@gmail.com>
2018-06-20 14:46:50 -0500

    u-boot: v2018.05-rc2
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -4,7 +4,7 @@ stable_at91bootstrap_sha="533f4082e35869ab61d1112f518063aa0496febb"
 
     uboot_old="v2018.01"
  uboot_stable="v2018.03"
-uboot_testing="v2018.05-rc1"
+uboot_testing="v2018.05-rc2"
 
-#uboot_prev="4cf5c5f1e60e6541630ae829b5fa2db704c268f0"
-#uboot_latest="47895838a4fbe867ba9a170c6d1ea6a794095025"
+#uboot_prev="8c0a17be0a632b9a823e14628c42f85c3e64f08e"
+#uboot_latest="8c0a17be0a632b9a823e14628c42f85c3e64f08e"
