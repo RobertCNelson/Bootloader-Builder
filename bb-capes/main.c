69e5b5da7583fb5273c5d4a7fe3d8c30ff29835d
Robert Nelson <robertcnelson@gmail.com>
2019-01-23 15:00:38 -0600

    am335x_evm: add BB-BONE-NH10C-01-00A0
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-capes/main.c
+++ bb-capes/main.c
@@ -35,6 +35,12 @@ int main (void)
 	cape_overlay_hash = hash_string(cape_overlay);
 	printf("[0x%lx]=[%s],[%s]\n", cape_overlay_hash, original_hash, cape_overlay);
 
+	//9dc
+	strlcpy(cape_overlay, "/lib/firmware/BB-BONE-NH10C-01-00A0.dtbo", 40 + 1);
+	strlcpy(original_hash, "0x9dcd73f", 9 + 1);
+	cape_overlay_hash = hash_string(cape_overlay);
+	printf("[0x%lx]=[%s],[%s]\n", cape_overlay_hash, original_hash, cape_overlay);
+
 	strlcpy(cape_overlay, "/lib/firmware/bb-justboom-amp-00A0.dtbo", 40 + 1);
 	strlcpy(original_hash, "0xb1b7bbf", 9 + 1);
 	cape_overlay_hash = hash_string(cape_overlay);
