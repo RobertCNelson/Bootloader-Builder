af7960c283480579901c7697e1eb6bf582d7ac96
Robert Nelson <robertcnelson@gmail.com>
2019-04-01 15:37:43 -0500

    am335x_evm: test another way to store diff
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -419,9 +419,82 @@ build_u_boot () {
 
 		case "${board}" in
 		am335x_evm)
-			echo "patch -p1 < \"${p_dir}/0001-am335x_evm-uEnv.txt-bootz-n-fixes.patch\""
-			echo "patch -p1 < \"${p_dir}/0002-U-Boot-BeagleBone-Cape-Manager.patch\""
+			#regenerate="enable"
+			if [ "x${regenerate}" = "xenable" ] ; then
+				base="../../patches/${uboot_old}/${board}/0001"
+
+				#reset="enable"
+				if [ "x${reset}" = "xenable" ] ; then
+					mkdir -p ${base}/arch/arm/include/asm/arch-am33xx/
+					cp arch/arm/include/asm/arch-am33xx/hardware_am33xx.h ${base}/arch/arm/include/asm/arch-am33xx/
+					cp arch/arm/include/asm/arch-am33xx/sys_proto.h ${base}/arch/arm/include/asm/arch-am33xx/
+
+					mkdir -p ${base}/arch/arm/mach-omap2/am33xx/
+					cp arch/arm/mach-omap2/am33xx/board.c ${base}/arch/arm/mach-omap2/am33xx/
+					cp arch/arm/mach-omap2/hwinit-common.c ${base}/arch/arm/mach-omap2/
+
+					mkdir -p ${base}/board/ti/am335x/
+					cp board/ti/am335x/board.c ${base}/board/ti/am335x/
+					cp board/ti/am335x/board.h ${base}/board/ti/am335x/
+					cp board/ti/am335x/mux.c ${base}/board/ti/am335x/
+
+					mkdir -p ${base}/configs/
+					cp configs/am335x_boneblack_defconfig ${base}/configs/
+					cp configs/am335x_evm_defconfig ${base}/configs/
+
+					mkdir -p ${base}/env/
+					cp env/common.c ${base}/env/
+
+					mkdir -p ${base}/include/configs/
+					cp include/configs/am335x_evm.h ${base}/include/configs/
+					cp include/configs/ti_armv7_common.h ${base}/include/configs/
+
+					mkdir -p ${base}/include/environment/ti/
+					cp include/environment/ti/mmc.h ${base}/include/environment/ti/
+
+					echo "patch -p1 < \"${p_dir}/0001-am335x_evm-uEnv.txt-bootz-n-fixes.patch\""
+					halt_patching_uboot
+				fi
+
+				cp -rv ${base}/* ./
+				git add --all
+				git commit -a -m 'am335x_evm: uEnv.txt, bootz, n fixes' -s
+				git format-patch -1 -o ../../patches/${uboot_old}/
+			fi
+
 			${git} "${p_dir}/0001-am335x_evm-uEnv.txt-bootz-n-fixes.patch"
+
+			#regenerate="enable"
+			if [ "x${regenerate}" = "xenable" ] ; then
+				base="../../patches/${uboot_old}/${board}/0002"
+
+				#reset="enable"
+				if [ "x${reset}" = "xenable" ] ; then
+					mkdir -p ${base}/arch/arm/mach-omap2/am33xx/
+					cp arch/arm/mach-omap2/am33xx/clock_am33xx.c ${base}/arch/arm/mach-omap2/am33xx/
+
+					mkdir -p ${base}/board/ti/am335x/
+					cp board/ti/am335x/board.c ${base}/board/ti/am335x/
+					cp board/ti/am335x/board.h ${base}/board/ti/am335x/
+					cp board/ti/am335x/mux.c ${base}/board/ti/am335x/
+
+					mkdir -p ${base}/include/configs/
+					cp include/configs/ti_armv7_common.h ${base}/include/configs/
+					cp include/configs/ti_armv7_omap.h ${base}/include/configs/
+
+					mkdir -p ${base}/include/environment/ti/
+					cp include/environment/ti/mmc.h ${base}/include/environment/ti/
+
+					echo "patch -p1 < \"${p_dir}/0002-U-Boot-BeagleBone-Cape-Manager.patch\""
+					halt_patching_uboot
+				fi
+
+				cp -rv ${base}/* ./
+				git add --all
+				git commit -a -m 'U-Boot: BeagleBone Cape Manager' -s
+				git format-patch -2 -o ../../patches/${uboot_old}/
+			fi
+
 			${git} "${p_dir}/0002-U-Boot-BeagleBone-Cape-Manager.patch"
 			;;
 		am335x_boneblack)
