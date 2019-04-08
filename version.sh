a008dc6be9395d75053821ae8b6efdcb6b4f04f0
Robert Nelson <robertcnelson@gmail.com>
2019-04-08 13:29:55 -0500

    rip out: git next
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -3,6 +3,3 @@
     uboot_old="v2018.09"
  uboot_stable="v2019.01"
 uboot_testing="v2019.04-rc2"
-
-#uboot_prev="8c0a17be0a632b9a823e14628c42f85c3e64f08e"
-#uboot_latest="8c0a17be0a632b9a823e14628c42f85c3e64f08e"
